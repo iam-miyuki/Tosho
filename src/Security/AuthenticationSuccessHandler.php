c43334ac8ac9c2c0e9339fbf168160aa23dc9674
Miyuki <miyuki@mail.com>
2025-08-28 16:53:47 +0200

    redirection after login

---
+++ src/Security/AuthenticationSuccessHandler.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace App\Security;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+
+
+// redirection apres login selon role. à voir aussi services.yaml et secuity.yaml
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $roles = $token->getRoleNames();
+
+        if (in_array('ROLE_ADMIN', $roles, true)) {
+            return new RedirectResponse($this->router->generate('admin_home'));
+        }
+
+        if (in_array('ROLE_LIBRARIEN', $roles, true)) {
+            return new RedirectResponse($this->router->generate('librarien'));
+        }
+
+        return new RedirectResponse($this->router->generate('account'));
+    }
+}
